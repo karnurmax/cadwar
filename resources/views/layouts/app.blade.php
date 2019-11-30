f0e3beb04f9fe22f7eb59394ee5c82ea596f81a7
karnurmax <karnurmax@mail.ru>
2019-11-30 22:52:28 +0500

    oauth: remove test rows

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,11 +76,6 @@
             @yield('content')
         </main>
 
-        <div class="col-md-12">
-            <passport-clients></passport-clients>
-            <passport-authorized-clients></passport-authorized-clients>
-            <passport-personal-access-tokens></passport-personal-access-tokens>
-        </div>
     </div>
 </body>
 </html>
