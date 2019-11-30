332ffeec7366eb14d1a414a3e7e93f0fb76354d3
karnurmax <karnurmax@mail.ru>
2019-11-30 22:46:29 +0500

    Authenticate: laravel standart auth (route + controller+ views)

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,19 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/artisan/migraterefresh','ArtisanController@MigrateRefresh');
+Route::get('/artisan/migraterefresh', 'ArtisanController@MigrateRefresh');
+
+$allowedToRegister = true; //TODO: env (allowedToRegister)
+if ($allowedToRegister) {
+    Auth::routes();
+} else {
+    Auth::routes(['register' => false]);
+}
+
+Route::get('/home', 'HomeController@index')->name('home');
