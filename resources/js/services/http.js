c045693e4a3bfedce90f6ebff7aa9ae9f5cd2d00
karnurmax <karnurmax@mail.ru>
2019-12-01 23:28:15 +0500

    http.js: catch err

--- resources/js/services/http.js
+++ resources/js/services/http.js
@@ -1,17 +1,26 @@
-import axios from 'axios';
-import apiUrl from './apiUrls';
+import axios from "axios";
+import apiUrl from "./apiUrls";
 axios.defaults.baseURL = apiUrl.API_URL;
+
 export default {
     get(url) {
-        return axios.get(url);
+        return axios.get(url).catch(err => {
+            console.error(err);
+        });
     },
     post(url, data) {
-        return axios.post(url, data);
+        return axios.post(url, data).catch(err => {
+            console.error(err);
+        });
     },
     put(url, data) {
-        return axios.put(url, data);
+        return axios.put(url, data).catch(err => {
+            console.error(err);
+        });
     },
     remove(url) {
-        return axios.delete(url);
+        return axios.delete(url).catch(err => {
+            console.error(err);
+        });
     }
-}
+};
