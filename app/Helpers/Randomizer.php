415774d8256ab40d37a784279b4cd8a61aeac529
karnurmax <karnurmax@mail.ru>
2019-12-01 22:14:07 +0500

    app/Helpers/Randomizer.php: GetString

---
+++ app/Helpers/Randomizer.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Helpers;
+
+class Randomizer
+{
+    public static function GetString($length)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+}
