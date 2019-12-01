1fb830dde35cc7e3097d64c0a606f6e6ed3c21c1
karnurmax <karnurmax@mail.ru>
2019-12-01 11:27:36 +0500

    routes => routes with children

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,17 +1,43 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
+<div>
+    <b-navbar toggleable="lg" type="light" variant="info">
+    <b-navbar-brand href="#">CADWAR</b-navbar-brand>
 
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+            <b-nav-item href="#">Базы</b-nav-item>
+            <b-nav-item href="#">Участники</b-nav-item>
+            <b-nav-item href="#">Истории</b-nav-item>
+            </b-navbar-nav>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+                <b-form-input size="sm" class="mr-sm-2" placeholder="Поиск"></b-form-input>
+                <b-button size="sm" class="my-2 my-sm-0" type="submit">Поиск</b-button>
+            </b-nav-form>
+
+            <b-nav-item-dropdown text="Язык" right>
+                <b-dropdown-item href="#">RU</b-dropdown-item>
+                <b-dropdown-item href="#">Скоро..)</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item-dropdown right>
+                <!-- Using 'button-content' slot -->
+                <template v-slot:button-content>
+                <em>Пользователь</em>
+                </template>
+                <b-dropdown-item href="#">Профиль</b-dropdown-item>
+                <b-dropdown-item href="#">Выйти</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+        </b-navbar-nav>
+    </b-collapse>
+    </b-navbar>
+    <router-view></router-view>
+</div>
 </template>
 
 <script>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,3 +1,18 @@
 export default [
-    { path: '/', component: require('./components/ExampleComponent.vue').default },
+    {
+        path: '/', component: require('./components/App.vue').default,
+        children: [
+            { path: '/databases', component: require('./components/Databases.vue').default },
+            { path: '/persons', component: require('./components/Persons.vue').default },
+            { path: '/histories', component: require('./components/Histories.vue').default },
+            { path: '/about', component: require('./components/About.vue').default },
+
+        ]
+    }, {
+        path: '/login', component: require('./components/Login.vue').default
+    }, {
+        path: '/register', component: require('./components/Register.vue').default
+    }, {
+        path: '*', component: require('./components/NotFound404.vue').default
+    },
 ]
