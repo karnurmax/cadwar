988e15073ba3ac2d913c15f42bdb44b246cc317b
karnurmax <karnurmax@mail.ru>
2019-12-02 11:12:23 +0500

    ...

--- public/js/app.js
+++ public/js/app.js
@@ -86367,10 +86367,10 @@ __webpack_require__.r(__webpack_exports__);
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony default export */ __webpack_exports__["default"] = ({
-  API_URL: 'http://localhost:8000/',
-  login: 'auth/login',
-  register: 'auth/register',
-  resetPassword: 'auth/reset'
+  API_URL: location.origin,
+  login: "auth/login",
+  register: "auth/register",
+  resetPassword: "auth/reset"
 });
 
 /***/ }),
