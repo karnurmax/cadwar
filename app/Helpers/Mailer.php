26d71ec580894fb4a5bd58d8a0ec7f8284d4cc5c
karnurmax <karnurmax@mail.ru>
2019-12-02 17:14:55 +0500

    Mailer:try to fix

--- app/Helpers/Mailer.php
+++ app/Helpers/Mailer.php
@@ -5,8 +5,12 @@ class Mailer
 {
     public static function Send(string $to, string $subject, string $message, $headers = null)
     {
-        $headers = 'MIME-Version: 1.0' . "\r\n";
-        $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
-        return mail($to, $subject, $message, $headers);
+        try {
+            $headers = 'MIME-Version: 1.0' . "\r\n";
+            $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+            return mail($to, $subject, $message, $headers);
+        } catch (Exception $ex) {
+            return false;
+        }
     }
 }
