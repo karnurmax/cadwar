8fa8632fb536daa9c645be9f2358581872fd3c74
karnurmax <karnurmax@mail.ru>
2019-12-02 11:30:53 +0500

    Mailer.php:  set content-type text/html

--- app/Helpers/Mailer.php
+++ app/Helpers/Mailer.php
@@ -3,8 +3,10 @@ namespace App\Helpers;
 
 class Mailer
 {
-    public static function Send(string $to, string $subject, string $message)
+    public static function Send(string $to, string $subject, string $message, $headers = null)
     {
-        return mail($to, $subject, $message);
+        $headers = 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=utf8' . "\r\n";
+        return mail($to, $subject, $message, $headers);
     }
 }
