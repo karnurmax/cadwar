93778609d1e6fe5f734c6ebd1dd7433db21c2860
Almas_TM <a.toktamys.a@mail.ru>
2019-12-26 15:10:30 +0600

    create migrations - databases, employees, histories and seed 2 of them done

---
+++ database/migrations/2019_12_26_035437_create_databases_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDatabasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('databases', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('address');
+            $table->bigInteger('members');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('databases');
+    }
+}
---
+++ database/seeds/DatabasesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+
+class DatabasesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('databases')->insert(
+            [
+            'name' => Str::random(10),
+            'address' => Str::random(15).' street 11',
+            'members' => 5,
+            'start_date' => Carbon::parse('2000-01-01'),
+            'end_date' => Carbon::parse('2010-01-01')
+            ]
+        );
+    }
+}
