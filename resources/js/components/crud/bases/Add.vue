219d8444da035fbe2c530bead7b643fa7a90d3b2
karnurmax <karnurmax@mail.ru>
2019-12-30 06:47:32 +0500

    databases Add.vue: add new item

---
+++ resources/js/components/crud/bases/Add.vue
@@ -0,0 +1,52 @@
+<template>
+    <b-modal
+        id="basesAddModal"
+        title="Добавление новой базы"
+        @ok="saveItem"
+    >
+        <b-form @submit="onSubmit">
+            <b-form-group
+                id="input-group-1"
+                label="Название :"
+                label-for="input-1"
+            >
+                <b-form-input
+                    id="input-1"
+                    v-model="item.name"
+                    type="text"
+                    required
+                    placeholder="Введите название"
+                ></b-form-input>
+            </b-form-group>
+
+            <template v-slot:modal-footer>
+                <b-button type="button">Отмена</b-button>
+                <b-button type="submit" variant="primary">Сохранить</b-button>
+            </template>
+        </b-form>
+    </b-modal>
+</template>
+
+<script>
+import crudService from '../../../services/crud';
+export default {
+    props: {
+        show: {
+            type: Boolean,
+            default: true
+        }
+    },
+    data() {
+        return {
+            item: {}
+        };
+    },
+    methods: {
+        onSubmit() {},
+        saveItem(e) {
+            e.preventDefault();
+            crudService.postNewItem('bases',this.item).then(res=>window.console.log(res));
+        }
+    }
+};
+</script>
