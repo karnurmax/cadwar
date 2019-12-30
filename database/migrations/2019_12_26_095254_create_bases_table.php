50ee7fd7289e1d59c8272ff8306bbb320509f492
karnurmax <karnurmax@mail.ru>
2019-12-30 06:37:00 +0500

    migrations/bases: alter column 'members' set default value to 0

--- database/migrations/2019_12_26_095254_create_bases_table.php
+++ database/migrations/2019_12_26_095254_create_bases_table.php
@@ -16,7 +16,7 @@ class CreateBasesTable extends Migration
         Schema::create('bases', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->bigInteger('members');
+            $table->bigInteger('members')->default(0);
             $table->timestamps();
         });
     }
