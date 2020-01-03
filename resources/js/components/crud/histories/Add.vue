6f1d19d83f89b0ffb9c98ca28c539d1c3213bc05
karnurmax <karnurmax@mail.ru>
2020-01-03 09:53:01 +0500

    crud/histories:copied from employees

---
+++ resources/js/components/crud/histories/Add.vue
@@ -0,0 +1,106 @@
+<template>
+    <b-modal
+        id="employeesAddModal"
+        title="Добавление новой базы"
+        @ok="saveItem"
+    >
+        <b-form @submit="onSubmit">
+            <b-form-group
+                id="input-group-1"
+                label="База данных:"
+                label-for="input-1"
+            >
+                <b-form-select @change="onDbChange" id="input-1">
+                    <option v-for="db in dbList" :value="db.id">{{ db.name }}</option>
+                </b-form-select>
+            </b-form-group>
+
+            <b-form-group
+                id="input-group-2"
+                label="Имя :"
+                label-for="input-2"
+            >
+                <b-form-input
+                    id="input-2"
+                    v-model="item.name"
+                    type="text"
+                    required
+                    placeholder="Имя"
+                ></b-form-input>
+            </b-form-group>
+            <b-form-group
+                id="input-group-3"
+                label="Фамилия :"
+                label-for="input-3"
+            >
+                <b-form-input
+                    id="input-3"
+                    v-model="item.surname"
+                    type="text"
+                    required
+                    placeholder="Фамилия"
+                ></b-form-input>
+            </b-form-group>
+            <b-form-group
+                id="input-group-4"
+                label="Отчество :"
+                label-for="input-4"
+            >
+                <b-form-input
+                    id="input-4"
+                    v-model="item.lastname"
+                    type="text"
+                    required
+                    placeholder="Отчество"
+                ></b-form-input>
+            </b-form-group>
+            <b-form-group
+                id="input-group-5"
+                label="ИИН :"
+                label-for="input-5"
+            >
+                <b-form-input
+                    id="input-5"
+                    v-model="item.iin"
+                    type="text"
+                    required
+                    placeholder="ИИН"
+                ></b-form-input>
+            </b-form-group>
+
+            <template v-slot:modal-footer>
+                <b-button type="button">Отмена</b-button>
+                <b-button type="submit" variant="primary">Сохранить</b-button>
+            </template>
+        </b-form>
+    </b-modal>
+</template>
+
+<script>
+import crudService from "../../../services/crud";
+export default {
+    props: ["dbList"],
+    data() {
+        return {
+            item: {}
+        };
+    },
+    methods: {
+        onSubmit() {},
+        saveItem(e) {
+            e.preventDefault();
+            crudService.postNewItem("employees", this.item).then(res => {
+                if (res.status === 200) {
+                    this.$emit("created", res.data);
+                    this.$bvModal.hide("employeesAddModal");
+                } else {
+                    window.alert("Ошибка");
+                }
+            });
+        },
+        onDbChange(dbItemId) {
+            this.item.base_id = dbItemId;
+        }
+    }
+};
+</script>
