6f5d5f4d79eb5e244bc3af42b2b8801a4a8986b6
karnurmax <karnurmax@mail.ru>
2020-01-03 20:56:53 +0500

    services/employee.js: call base fileupload service instead of customizing

--- resources/js/services/employee.js
+++ resources/js/services/employee.js
@@ -2,17 +2,9 @@ import http from "./http";
 import apiUrls from "./apiUrls";
 export default {
     uploadFile(userId, files) {
-        let formData = new FormData();
-        formData.append("files[]", files);
-
-        return http.post(
+        return http.uploadFile(
             apiUrls.employeeFilesUpload.replace("{id}", userId),
-            formData,
-            {
-                headers: {
-                    "Content-Type": "multipart/form-data"
-                }
-            }
+            files
         );
     }
 };
