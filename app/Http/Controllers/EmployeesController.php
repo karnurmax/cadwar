bb972ac033f43d672f505cd83c917c28715df387
karnurmax <karnurmax@mail.ru>
2020-01-03 21:27:11 +0500

    EmployeesController.php: handle files upload

---
+++ app/Http/Controllers/EmployeesController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \App\EmployeeFile;
+
+class EmployeesController extends Controller
+{
+    public function upload(Request $request, $id)
+    {
+        $files = $_FILES["files"];
+        $fnames = $files["name"];
+
+        for ($i = 0; $i < count($fnames); $i++) {
+            $tmpPath = $files["tmp_name"][$i];
+            $parent = dirname($_SERVER['DOCUMENT_ROOT']);
+            $parent .= "\\uploads\\";
+
+            $guid = \App\Helpers\Randomizer::GetString(20);
+            mkdir("../uploads/$guid");
+            $newPath = "../uploads/$guid/$fnames[$i]";
+            $moved = move_uploaded_file($tmpPath, $newPath);
+            if (!$moved) {
+                continue;
+            }
+            $empFile = new EmployeeFile;
+            $empFile->employee_id = $id;
+            $empFile->guid = $guid;
+            $empFile->filename = $newPath;
+            $empFile->save();
+        }
+        return "OK";
+    }
+
+}
